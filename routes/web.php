9c0ef0589395f7340daa3dbe93b01ce4aa4503c3
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-02 13:57:59 +0300

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,9 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('drivers', DriverController::class);
     Route::resource('payments', PaymentController::class);
     Route::resource('expenses', ExpenseController::class);
-    Route::resource('journeys', JourneyController   ::class);
-    Route::resource('client-participations', ClientParticipationController::class);
-    Route::get('/clients/{client}/latest-balance', [ParticipationController::class, 'latestBalance']);
-    Route::get('/clients/{client}/profile', [ClientController::class, 'profile'])->name('clients.profile');
 
 });
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
