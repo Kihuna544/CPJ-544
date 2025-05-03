38ae66185b022307a0342de11643111c721c7c9d
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-04 01:11:26 +0300

    B2tCLient Model MVP introduced

---
+++ app/Models/B2tClient.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class B2tClient extends Model
+{
+    //
+    protected $fillable= [
+        'b2t_trip_id',
+        'client_id',
+        'client_name',
+        'no_of_sacks_per_client',
+        'no_of_packages_per_client',
+        'amount_to_pay_for_b2t',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function b2tClient()
+    {
+        return $this->belongsToMany(Client::class, 'b2t_trip_clients');
+    }
+
+    public function b2tTrip()
+    {
+        return $this->belongsTo(B2tTrip::class);
+    }
+}
