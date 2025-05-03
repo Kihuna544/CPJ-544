a5dc083e107bd7b0244744d34a1e70fbc3a224d7
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-04 00:51:18 +0300

    B2tTrip model MVP inserted

---
+++ app/Models/B2tTrip.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class B2tTrip extends Model
+{
+    //
+    protected $fillable = [
+        'driver_id',
+        'trip_date',
+        'total_number_of_sacks',
+        'total_number_of_packages',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function b2tClient()
+    {
+        return $this->belongsTo(TemporaryClient::class, 'b2t_trip_clients');
+    }
+
+    public function drivers()
+    {
+        return $this->belongsTo(Driver::class);
+    }
+}
