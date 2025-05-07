1c7dc646b0df7011f2757fbb7eb1cf2cc8c90a63
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-07 06:10:42 +0300

    ClientController routes registered in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\DriverController;
+use App\Http\Contrllers\API\ClientController;
 
-// Optional test route
-Route::get('/ping', function () {
-    return response()->json(['message' => 'API is working']);
-});
 
-// Your actual Driver routes
+
 Route::apiResource('/drivers', DriverController::class);
+Route::apiResource('/clients', ClientController::class);
