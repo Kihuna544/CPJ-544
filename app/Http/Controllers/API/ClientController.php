661c3db61e1c93a25d1e4196bf50bfcbd8c4b507
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-07 06:07:54 +0300

    index, store and show methods empasized in ClientController

---
+++ app/Http/Controllers/API/ClientController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Contrllers\Controller;
+use App\Models\Client;
+use  Illuminate\Http\Request;
+
+class ClientController extends Controllers {
+
+    public function index() {
+        return Driver::all;
+    }
+
+    public function store(Request $request) {
+
+        $validated = $request->validate ([
+            'client_name' => 'required|string',
+            'phone' => 'required|string|unique:drivers, phone',
+            'profile_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'profile_photo_camera' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $photoPath = null;
+
+        if($request->hasFile('profile_photo'))
+        {
+            $photoPath = $request->file('profile_photo')->store('drivers', 'public');
+        }
+
+        elseif($request->hasFIle('profile_photo_camera'))
+        {
+            $photoPath = $request->file('profile_photo_camera')->store('drivers', 'public');
+        }
+
+        if($photoPath) 
+        {
+            $validated['profile_photo'] = $photoPath;
+        }
+
+        $drivers = Driver::create($validated);
+        return response()->json($client, 201);
+    }
+    
+    public function show($id)
+    {
+        return Client::findOrFail($id);
+    }  
+}
+
