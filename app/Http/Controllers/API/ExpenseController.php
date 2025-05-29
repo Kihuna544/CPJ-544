15d0801035884258825e56efe7f3fb1adfd5e008
Kihuna544 <khalfankhalifa544@gmail.com>
2025-05-29 13:26:16 +0300

    Trip ExpenseController MVP

---
+++ app/Http/Controllers/API/ExpenseController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Conrollers\Controller;
+use App\Models\Expense;
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $perPage = $request->query('per_page', 10);
+
+        $expense = Expense::with('trip')
+                ->latest()
+                ->paginate($perPage);
+
+    return response()->json($expense);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate
+        ([
+            'expense_date' => 'required|date',
+            'trip_id' => 'required|exists:trips,id',
+            'category' => 'required|string|max:255',
+            'amount' => 'required|numeric|min:0',
+            'notes' => '' // should be validated
+        ]);
+
+        $validated['created_by'] = auth()->id();
+        $expense = Expense::create($validated);
+
+        return response()->json($expense->load('trip'), 201);
+    }
+
+
+    public function update(Request $request, Expense $expense)
+    {
+        $validated = $request->validate
+        ([
+            'expense_date' => 'required|date',
+            'trip_id' => 'required|exists:trips,id',
+            'category' => 'required|string|max:255',
+            'amount' => 'required|numeric|min:0',
+            'notes' => '' // should be validated
+        ]);
+
+        $validated['updated_by'] = auth()->id();
+        $expense->update($validated);
+
+        return response()->json($expense->load('trip'), 200);
+    }
+
+
+    public function show(Expense $expense)
+    {
+        return response()->json($expense->load('trip'));
+    }
+
+
+    public function destroy(Expense $expense)
+    {
+        $expense->delete();
+
+        return response()->json
+        ([
+            'message' => 'Expese successfully deleted',
+            'expense' => $expense->load('trip')
+        ]);
+    }
+}
